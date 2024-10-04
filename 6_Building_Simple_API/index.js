d8c82c0690ef5849b03792129841047d30fb10bb
Snehanshu Vivek <snehanshuvivek123@gmail.com>
2024-10-04 23:07:45 +0530

    Add API to the server.

---
+++ 6_Building_Simple_API/index.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const fs = require('fs');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+    const pathName = req.url;
+    if (pathName === '/' || pathName === '/overview') {
+        res.end('This is the overview');
+    } else if (pathName === '/product') {
+        res.end('This is the product');
+    } else if (pathName === '/api') {
+        res.writeHead(200, {'Content-type': 'application/json'});
+        res.end(data);
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found</h1>');
+    }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000');
+});
+
+/*
+To request api to server use following on web browser
+http://127.0.0.1:8000/api => This will give back data present on server.
+Here the data is present on dev-data/data.json.
+All the data is loaded at the startup in blocking manner.
+Whenever a client requests for data, it responds with the data present on server.
+*/
